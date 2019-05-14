a0f4d32e92b56d7806725876eadf433189f9f353
Mike Liu <mike.liu.dev@gmail.com>
2019-05-13 22:33:25 -0400

    Use real users for login view.
    Flask-Login requires a loader to get a user to log in. We can now also
    use flask_login functions to log users in and check their logged in
    status.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,8 +4,12 @@ from flask import (
     redirect,
     url_for
 )
+
+from flask_login import current_user, login_user
+
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -27,9 +31,23 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    # User already logged in.
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    # Handle for submit
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # Invalid password.
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        # Log the user in.
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
     return render_template('login.html', title='Sign In', form=form)
