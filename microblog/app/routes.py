4db2963eb30e0cf6f564db77f41f81da10ba079c
Mike Liu <mike.liu.dev@gmail.com>
2019-05-13 22:43:47 -0400

    Require users to login for views and allow them to logout.
    Requiring users to login means redirecting users to the login view when
    attempting to access a page that requires logging in. Redirecting users
    to the login view interrupts the user's workflow which means we should
    be nice and redirect them back to the page they were trying to access
    when they were interrupted. In order to do this we need to handle the
    next argument provided to the request query parameters in the login
    route.
    
    This commit also provides user's a way to logout. The logout button is
    context dependent on the current_user's logged in state.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -2,10 +2,18 @@ from flask import (
     render_template,
     flash,
     redirect,
-    url_for
+    url_for,
+    request
 )
 
-from flask_login import current_user, login_user
+from flask_login import (
+    current_user,
+    login_user,
+    logout_user,
+    login_required
+)
+
+from werkzeug.urls import url_parse
 
 from app import app
 from app.forms import LoginForm
@@ -14,6 +22,7 @@ from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -48,6 +57,21 @@ def login():
 
         # Log the user in.
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+
+        # Get next page if redirected here from login_required function
+        next_page = request.args.get('next')
+
+        # Check if next page exists and for security check if next_page is
+        # a relative link. If not then redirect to the index (naughty hacker).
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
 
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
