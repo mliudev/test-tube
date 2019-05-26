607a8c036a1dd78e75905d79d1a979b7a06901a9
Mike Liu <mike.liu.dev@gmail.com>
2019-05-26 11:10:46 -0500

    Create a view that allows users to edit their profile.
    This view is a form that let's users change their username as well as
    populate their bio field. Users can edit their profile using a new link
    in their profile page.

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,11 +1,20 @@
 from flask_wtf import FlaskForm
+
 from wtforms import (
     StringField,
+    TextAreaField,
     PasswordField,
     BooleanField,
     SubmitField
 )
-from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+
+from wtforms.validators import (
+    DataRequired,
+    Email,
+    ValidationError,
+    EqualTo,
+    Length
+)
 
 from .models import User
 
@@ -34,3 +43,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -18,7 +18,7 @@ from flask_login import (
 from werkzeug.urls import url_parse
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 
@@ -89,6 +89,25 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template(
+        'edit_profile.html', title="Edit Profile", form=form)
 
 
 @app.route('/logout')
