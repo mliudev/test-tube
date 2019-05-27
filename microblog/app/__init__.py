d1bdda8de92efa2fd8b83682b941bf708b0c2bac
Mike Liu <mike.liu.dev@gmail.com>
2019-05-26 23:26:25 -0400

    Setup a file logger.
    Creates a logs directory and logs at the INFO level to 'microblog.log'
    file.

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
+import os
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
 
@@ -44,3 +45,22 @@ if not app.debug:
 
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+        # ensure log directory exists
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+
+        # setup a file handler logger
+        file_handler = RotatingFileHandler(
+            'logs/microblog.log',
+            maxBytes=10240,
+            backupCount=10)
+
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: '
+            '%(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
