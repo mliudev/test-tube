873691d807b9b99fcb03b274d4727998f00af3b9
Mike Liu <mike.liu.dev@gmail.com>
2019-05-26 23:39:25 -0400

    Fix duplicate username error.
    Prevents users from changing their username to an existing one. This is
    not thread-safe.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -94,7 +94,7 @@ def register():
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
 
     if form.validate_on_submit():
         current_user.username = form.username.data
