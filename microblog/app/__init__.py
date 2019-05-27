84fd2bf8a56b55ae05fcaa14185438ed2ae8dac2
Mike Liu <mike.liu.dev@gmail.com>
2019-05-27 17:12:48 -0400

    Create password reset mechanism.
    Uses JSON web tokens and flask mail to generate password reset emails.

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 from config import Config
 
@@ -15,6 +16,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
