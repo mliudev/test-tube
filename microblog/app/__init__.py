3c221138aa59d25e998ba0b5a066d531eadba8d9
Mike Liu <mike.liu.dev@gmail.com>
2019-05-26 23:18:44 -0400

    Log errors to email in production.
    Uses environment variables to set mail server parameters for emailing
    admins when an exception occurs. This happens only in production.

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,4 +1,6 @@
 # flake8: noqa
+import logging
+from logging.handlers import SMTPHandler
 
 from flask import Flask
 
@@ -17,3 +19,28 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        # configure auth credentials
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+
+        # configure security
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+
+        # setup mail handler
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Microblog Failure',
+            credentials=auth,
+            secure=secure,
+            timeout=10)
+
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
--- microblog/config.py
+++ microblog/config.py
@@ -9,3 +9,11 @@ class Config(object):
         'DATABASE_URL',
         'sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # e-mail notifications
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USER_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['mike.liu@mliu.io']
