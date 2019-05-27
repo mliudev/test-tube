aab51be5cbf917be56249663ac0eb4762d67414a
Mike Liu <mike.liu.dev@gmail.com>
2019-05-27 14:43:58 -0400

    Allow users to submit blog posts.
    Creates a text field in the main page for users to submit blog posts.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -18,14 +18,23 @@ from flask_login import (
 from werkzeug.urls import url_parse
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
+    # test data
     posts = [
         {
             'author': {'username': 'Mike'},
@@ -36,7 +45,8 @@ def index():
             'body': 'Endgame is amazing!'
         }
     ]
-    return render_template('index.html', posts=posts)
+    return render_template(
+        "index.html", title='Home Page', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
