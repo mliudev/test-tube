e061196acf14014d23422e78bdf51d90a980cd3f
Mike Liu <mike.liu.dev@gmail.com>
2019-05-27 17:16:37 -0400

    Make sending mail asynchronous.
    Sending mail is a slow process that should be done in a background
    thread from the main web app.

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -1,14 +1,9 @@
+from threading import Thread
+
 from flask import render_template
 from flask_mail import Message
 
 from app import mail, app
-
-
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    mail.send(msg)
 
 
 def send_password_reset_email(user):
@@ -20,3 +15,15 @@ def send_password_reset_email(user):
                                          user=user, token=token),
                html_body=render_template('email/reset_password.html',
                                          user=user, token=token))
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
