da4ed331511554207921a1261d24434180aa322d
Mike Liu <mike.liu.dev@gmail.com>
2019-05-27 11:57:04 -0400

    Create followers model.
    A user follows other users and is followed by many other users. This
    requires an association table for the many-to-many relationship.

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -14,6 +14,14 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+# association tables
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -22,6 +30,11 @@ class User(db.Model, UserMixin):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
